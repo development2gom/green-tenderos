4a4bc8bd8c93e32cb5b175a9dc7ded42a73bfd20
Raul Alanis <rulosc07@gmail.com>
2018-09-04 09:27:49 -0500

    permisos de usuarios y datos de puntuaciones de tienda

--- views/site/puntuacion.php
+++ views/site/puntuacion.php
@@ -8,10 +8,12 @@
     </div>
     <div class="col-8 col-sm-12 col-md-8 col-lg-8">
         <h5><?= $tienda->txt_nombre ?></h5>
+        <p>Actual: <?= $puntuajeActual->num_puntuaje_actual ?></p>
         <p>Antes: <?= $puntuajeActual->num_saldo_anterior ?></p>
         <p>Mes: <?= $puntuajeActual->num_saldo_mes ?></p>
         <p>Total: <?= $puntuajeActual->num_saldo_acumulado ?></p>
         <p>Nivel: <?= $puntuajeActual->nivel->txt_nombre ?></p>
+        <p>Siguiente nivel: <?= $puntuajeActual->num_puntos_sig_experiencia ?></p>
     </div>
 </div>
 
