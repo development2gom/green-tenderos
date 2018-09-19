5bf1ed000fb449ccf9619f5e7bfdcda78f145ca0
Damián <damian2gom@gmail.com>
2018-09-19 11:35:03 -0500

    testimonios

--- views/site/testimonios.php
+++ views/site/testimonios.php
@@ -1,1 +1,60 @@
-testimonios
+<?php
+use yii\helpers\Url;
+/* @var $this yii\web\View */
+
+$this->title = "Testimonios";
+
+$this->params['classBody'] = "testimonios";
+// site-navbar-small site-top
+?>
+
+<div class="contend testimonios-contend">
+
+    <div class="testimonios-title">
+        <img src="<?=Url::base()?>/webAssets/images/logo-cdg.png" alt="">
+        <h2>
+            testimonios
+        </h2>
+    </div>
+    
+    <div class="testimonios-textos">
+        
+        <div class="testimonios-imagenes-col">
+
+            <div class="testimonios-imagenes-col-a">
+                <img src="https://via.placeholder.com/150x100" alt="">
+            </div>
+            <div class="testimonios-imagenes-col-a">
+                <img src="https://via.placeholder.com/150x100" alt="">
+            </div>
+            <div class="testimonios-imagenes-col-a">
+                <img src="https://via.placeholder.com/150x100" alt="">
+            </div>
+            <div class="testimonios-imagenes-col-a">
+                <img src="https://via.placeholder.com/150x100" alt="">
+            </div>
+            <div class="testimonios-imagenes-col-a">
+                <img src="https://via.placeholder.com/150x100" alt="">
+            </div>
+            <div class="testimonios-imagenes-col-a">
+                <img src="https://via.placeholder.com/150x100" alt="">
+            </div>
+
+        </div>
+        <div class="testimonios-videos-col">
+
+            <div class="testimonios-imagenes-col-b">
+                <img src="https://via.placeholder.com/300x150" alt="">
+            </div>
+            <div class="testimonios-imagenes-col-b">
+                <img src="https://via.placeholder.com/300x150" alt="">
+            </div>
+            <div class="testimonios-imagenes-col-b">
+                <img src="https://via.placeholder.com/300x150" alt="">
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
