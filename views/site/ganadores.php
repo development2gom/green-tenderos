bf48e7f9b4a662244a8e81ff9bf52514c488db7a
Damián <damian2gom@gmail.com>
2018-09-19 09:57:04 -0500

    Ganadores

--- views/site/ganadores.php
+++ views/site/ganadores.php
@@ -1,1 +1,44 @@
-ganadores
+<?php
+use yii\helpers\Url;
+/* @var $this yii\web\View */
+
+$this->title = "Ganadores";
+
+$this->params['classBody'] = "ganadores";
+// site-navbar-small site-top
+?>
+
+<div class="contend ganadores-contend">
+
+    <div class="ganadores-title">
+        <img src="<?=Url::base()?>/webAssets/images/logo-cdg.png" alt="">
+        <h2>
+            <span>círculo</span>
+            <span>de</span>
+            <span>ganadores</span>
+        </h2>
+    </div>
+    
+    <div class="ganadores-textos">
+        <h3>¡GANASTE!</h3>
+        <div class="ganadores-textos-table">
+
+            <div class="ganadores-textos-table-td">
+                <p>. Rubén Vázquez Galván</p>
+                <p>. Marina Aguilar Trejo</p>
+                <p>. Eduardo Méndez González</p>
+                <p>. Roxana Reyes Herrera</p>
+                <p>. Jimena Hernández</p>
+            </div>
+            <div class="ganadores-textos-table-td">
+                <p>. Ernesto García Cuevas</p>
+                <p>. Lucía Fernanda Pérez</p>
+                <p>. Mario Santos López</p>
+                <p>. Gabriel Medina Díaz</p>
+                <p>. Máximo Santana López</p>
+            </div>
+                
+        </div>
+    </div>
+
+</div>
