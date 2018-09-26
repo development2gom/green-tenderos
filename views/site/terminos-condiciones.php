a4885e9ddab64d9269e34d7b5be950b0974aabfb
Damián <damian2gom@gmail.com>
2018-09-26 10:10:14 -0500

    Ajuste de términos y condiciones

--- views/site/terminos-condiciones.php
+++ views/site/terminos-condiciones.php
@@ -21,10 +21,301 @@ $this->params['classBody'] = "terminos-condiciones";
     
     <div class="tc-textos">
 
-        <a href="<?=Url::base()?>/webAssets/pdf/terminos-condiciones.pdf" target="blank" class="tc-textos-item">
+        <!-- <a href="<?=Url::base()?>/webAssets/pdf/terminos-condiciones.pdf" target="blank" class="tc-textos-item">
             <img src="<?=Url::base()?>/webAssets/images/pdf.png" alt="">
             <span>Descargar Término y Condiciones</span>
-        </a>
+        </a> -->
+        
+        <div class="ap-textos-desc">
+
+            <p class="p1"><span class="s1"><strong>TÉRMINOS Y CONDICIONES</strong></span></p>
+        
+            <p class="p3"><span class="s1"><strong>Lorem ipsum</strong></span></p>
+        
+        </div>
+
+        <div class="tc-textos-table">
+            <div class="tc-textos-table-head">
+                <div class="tc-textos-table-th">
+                    Estado
+                </div>
+                <div class="tc-textos-table-th">
+                    Nº Permiso
+                </div>
+            </div>
+
+            <div class="tc-textos-table-body">
+            
+                <div class="tc-textos-table-tr">
+                    <div class="tc-textos-table-td">
+                        <p>Guerrero</p>
+                    </div>
+                    <div class="tc-textos-table-td">
+                        <p>20180268PS03</p>
+                    </div>
+                </div>
+                <div class="tc-textos-table-tr">
+                    <div class="tc-textos-table-td">
+                        <p>Morelos
+                    </div>
+                    <div class="tc-textos-table-td">
+                        <p>20180270PS08
+                    </div>
+                </div>
+                <div class="tc-textos-table-tr">
+                    <div class="tc-textos-table-td">
+                        <p>Oaxaca</p>
+                    </div>
+                    <div class="tc-textos-table-td">
+                        <p>20180269PS07</p>
+                    </div>
+                </div>
+                <div class="tc-textos-table-tr">
+                    <div class="tc-textos-table-td">
+                        <p>Puebla / Tlaxcala</p>
+                    </div>
+                    <div class="tc-textos-table-td">
+                        <p>20180271PS02</p>
+                    </div>
+                </div>
+                <div class="tc-textos-table-tr">
+                    <div class="tc-textos-table-td">
+                        <p>Veracruz Centro</p>
+                    </div>
+                    <div class="tc-textos-table-td">
+                        <p>20180272PS05</p>
+                    </div>
+                </div>
+                <div class="tc-textos-table-tr">
+                    <div class="tc-textos-table-td">
+                        <p>Veracruz Puerto</p>
+                    </div>
+                    <div class="tc-textos-table-td">
+                        <p>20180274PS02</p>
+                    </div>
+                </div>
+                <div class="tc-textos-table-tr">
+                    <div class="tc-textos-table-td">
+                        <p>Veracruz Sur</p>
+                    </div>
+                    <div class="tc-textos-table-td">
+                        <p>20180276PS01</p>
+                    </div>
+                </div>
+
+
+                <div class="tc-textos-table-tr">
+                    <div class="tc-textos-table-td">
+                        <p>CENTRO</p>
+                    </div>
+                    <div class="tc-textos-table-td">
+                        <p>20180279PS00</p>
+                    </div>
+                </div>
+                <div class="tc-textos-table-tr">
+                    <div class="tc-textos-table-td">
+                        <p>HIDALGO</p>
+                    </div>
+                    <div class="tc-textos-table-td">
+                        <p>20180281PS02</p>
+                    </div>
+                </div>
+                <div class="tc-textos-table-tr">
+                    <div class="tc-textos-table-td">
+                        <p>ORIENTE/ C.HISTORICO</p>
+                    </div>
+                    <div class="tc-textos-table-td">
+                        <p>20180282PS01</p>
+                    </div>
+                </div>
+                <div class="tc-textos-table-tr">
+                    <div class="tc-textos-table-td">
+                        <p>PONIENTE</p>
+                    </div>
+                    <div class="tc-textos-table-td">
+                        <p>20180277PS03</p>
+                    </div>
+                </div>
+                <div class="tc-textos-table-tr">
+                    <div class="tc-textos-table-td">
+                        <p>SUR</p>
+                    </div>
+                    <div class="tc-textos-table-td">
+                        <p>20180278PS09</p>
+                    </div>
+                </div>
+
+
+                <div class="tc-textos-table-tr">
+                    <div class="tc-textos-table-td">
+                        <p>MONTERREY</p>
+                    </div>
+                    <div class="tc-textos-table-td">
+                        <p>20180308PS00</p>
+                    </div>
+                </div>
+                <div class="tc-textos-table-tr">
+                    <div class="tc-textos-table-td">
+                        <p>COAH-DGO</p>
+                    </div>
+                    <div class="tc-textos-table-td">
+                        <p>20180283PS09</p>
+                    </div>
+                </div>
+                <div class="tc-textos-table-tr">
+                    <div class="tc-textos-table-td">
+                        <p>SLP-ZAC</p>
+                    </div>
+                    <div class="tc-textos-table-td">
+                        <p>20180284PS04</p>
+                    </div>
+                </div>
+                <div class="tc-textos-table-tr">
+                    <div class="tc-textos-table-td">
+                        <p>FRONTERA</p>
+                    </div>
+                    <div class="tc-textos-table-td">
+                        <p>20180285PS03</p>
+                    </div>
+                </div>
+                <div class="tc-textos-table-tr">
+                    <div class="tc-textos-table-td">
+                        <p>GOLFO</p>
+                    </div>
+                    <div class="tc-textos-table-td">
+                        <p>20180286PS00</p>
+                    </div>
+                </div>
+
+
+                <div class="tc-textos-table-tr">
+                    <div class="tc-textos-table-td">
+                        <p>CELAYA </p>
+                    </div>
+                    <div class="tc-textos-table-td">
+                        <p>20180289PS09</p>
+                    </div>
+                </div>
+                <div class="tc-textos-table-tr">
+                    <div class="tc-textos-table-td">
+                        <p>COSTA-JAL-NAY</p>
+                    </div>
+                    <div class="tc-textos-table-td">
+                        <p>20180288PS08</p>
+                    </div>
+                </div>
+                <div class="tc-textos-table-tr">
+                    <div class="tc-textos-table-td">
+                        <p>GUADALAJARA</p>
+                    </div>
+                    <div class="tc-textos-table-td">
+                        <p>20180287PS00</p>
+                    </div>
+                </div>
+                <div class="tc-textos-table-tr">
+                    <div class="tc-textos-table-td">
+                        <p>JALISCO CENTRO</p>
+                    </div>
+                    <div class="tc-textos-table-td">
+                        <p>20180290PS01</p>
+                    </div>
+                </div>
+                <div class="tc-textos-table-tr">
+                    <div class="tc-textos-table-td">
+                        <p>LEON</p>
+                    </div>
+                    <div class="tc-textos-table-td">
+                        <p>20180291PS05</p>
+                    </div>
+                </div>
+                <div class="tc-textos-table-tr">
+                    <div class="tc-textos-table-td">
+                        <p>MICHOACAN</p>
+                    </div>
+                    <div class="tc-textos-table-td">
+                        <p>20180292PS04</p>
+                    </div>
+                </div>
+
+                
+                <div class="tc-textos-table-tr">
+                    <div class="tc-textos-table-td">
+                        <p>BAJA NORTE</p>
+                    </div>
+                    <div class="tc-textos-table-td">
+                        <p>20180293PS09</p>
+                    </div>
+                </div>
+                <div class="tc-textos-table-tr">
+                    <div class="tc-textos-table-td">
+                        <p>CHIHUAHUA</p>
+                    </div>
+                    <div class="tc-textos-table-td">
+                        <p>20180294PS08</p>
+                    </div>
+                </div>
+                <div class="tc-textos-table-tr">
+                    <div class="tc-textos-table-td">
+                        <p>SINALOA</p>
+                    </div>
+                    <div class="tc-textos-table-td">
+                        <p>20180295PS06</p>
+                    </div>
+                </div>
+                <div class="tc-textos-table-tr">
+                    <div class="tc-textos-table-td">
+                        <p>SONORA</p>
+                    </div>
+                    <div class="tc-textos-table-td">
+                        <p>20180297PS04</p>
+                    </div>
+                </div>
+
+
+                <div class="tc-textos-table-tr">
+                    <div class="tc-textos-table-td">
+                        <p>CHIAPAS ALTOS</p>
+                    </div>
+                    <div class="tc-textos-table-td">
+                        <p>20180296PS04</p>
+                    </div>
+                </div>
+                <div class="tc-textos-table-tr">
+                    <div class="tc-textos-table-td">
+                        <p>CHIAPAS COSTA</p>
+                    </div>
+                    <div class="tc-textos-table-td">
+                        <p>20180298PS08</p>
+                    </div>
+                </div>
+                <div class="tc-textos-table-tr">
+                    <div class="tc-textos-table-td">
+                        <p>QUINTANA ROO</p>
+                    </div>
+                    <div class="tc-textos-table-td">
+                        <p>20180299PS09</p>
+                    </div>
+                </div>
+                <div class="tc-textos-table-tr">
+                    <div class="tc-textos-table-td">
+                        <p>TABASCO</p>
+                    </div>
+                    <div class="tc-textos-table-td">
+                        <p>20180300PS02</p>
+                    </div>
+                </div>
+                <div class="tc-textos-table-tr">
+                    <div class="tc-textos-table-td">
+                        <p>YUCATAN</p>
+                    </div>
+                    <div class="tc-textos-table-td">
+                        <p>20180301PS02</p>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
 
     </div>
 
