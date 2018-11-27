2c2eea9d48ae8f9f746892f9af54a276e28482a0
Damián <damian2gom@gmail.com>
2018-11-27 13:26:22 -0600

    Ajustes

--- views/site/testimonios.php
+++ views/site/testimonios.php
@@ -42,62 +42,76 @@ $this->registerJsFile(
     
     <div class="testimonios-textos">
         
-        <div class="testimonios-imagenes-col">
-        <?php
-        foreach($imagenes as $imagen){
-            
-        ?>
-              
-            <div class="testimonios-imagenes-col-a">
-                <figure class="card-img-top overlay-hover overlay">
-                    <img src="<?= Constantes::URL_ADMIN . "/" . $imagen->txt_url?>" alt="">
-                    <figcaption class="overlay-panel overlay-background overlay-fade overlay-icon">
-                        <a class="icon wb-search" href="<?= Constantes::URL_ADMIN . "/" . $imagen->txt_url?>"></a>
-                    </figcaption>
-                </figure>
+        <div class="testimonios-imagenes-row">
+
+            <div class="testimonios-subtitle">
+                <h3>Imágenes</h3>
             </div>
-           
-        <?php
-           }
-        ?>
+
+            <div class="testimonios-imagenes-col">
+                <?php
+                foreach($imagenes as $imagen){
+                    
+                ?>
+                    
+                    <div class="testimonios-imagenes-col-a">
+                        <figure class="card-img-top overlay-hover overlay">
+                            <img src="<?= Constantes::URL_ADMIN . "/" . $imagen->txt_url?>" alt="">
+                            <figcaption class="overlay-panel overlay-background overlay-fade overlay-icon">
+                                <a class="icon wb-image" href="<?= Constantes::URL_ADMIN . "/" . $imagen->txt_url?>"></a>
+                            </figcaption>
+                        </figure>
+                    </div>
+                
+                <?php
+                }
+                ?>
+            </div>
+
         </div>
 
-        <div class="testimonios-videos-col">
-            <?php
-            foreach($videos as $video){
-                
-            ?>
-                        <div class="testimonios-imagenes-col-b">
-                            <?php
-                            /**
-                             * Separar url de youtube y el id del video
-                             */
-                            // $arrayUrl = explode('=', $video->txt_url);
-                            ?>
-                            <!-- <iframe width="420" height="315"
-                                src="https://www.youtube.com/embed/<?php // $arrayUrl[1] ?>">
-                            </iframe> -->
+        <div class="testimonios-videos-row">
 
-
-                            <figure class="card-img-top overlay-hover overlay">
-                                <video class="overlay-video overlay-figure overlay-scale" style="background-image: url('http://img.youtube.com/vi/<?= EntVideos::getIdVideoYoutube($video->txt_url) ?>/mqdefault.jpg') ">
-                                    <source src="http://img.youtube.com/vi/<?= EntVideos::getIdVideoYoutube($video->txt_url) ?>/mqdefault.jpg">
-                                    <!-- <source src="video.ogg" type="video/ogg">
-                                    <source src="video.webm" type="video/webm"> -->
-                                    Tu navegar no soporta la etiqueta de video.
-                                </video> 
-
-                                <figcaption class="overlay-panel overlay-background overlay-fade overlay-icon">
-                                    <a class="icon wb-search mfp-iframe" href="<?= $video->txt_url ?>"></a>
-                                    <p class="card-block"><?= $video->txt_nombre ?></p>
-                                </figcaption>
-                            </figure>
-                        </div>
+            <div class="testimonios-subtitle">
+                <h3>Vídeos</h3>
+            </div>
             
+            <div class="testimonios-videos-col">
+                <?php
+                foreach($videos as $video){
+                    
+                ?>
+                    <div class="testimonios-imagenes-col-b">
+                        <?php
+                        /**
+                         * Separar url de youtube y el id del video
+                         */
+                        // $arrayUrl = explode('=', $video->txt_url);
+                        ?>
+                        <!-- <iframe width="420" height="315"
+                            src="https://www.youtube.com/embed/<?php // $arrayUrl[1] ?>">
+                        </iframe> -->
+
+                        <figure class="card-img-top overlay-hover overlay">
+                            <video class="overlay-video overlay-figure overlay-scale" style="background-image: url('http://img.youtube.com/vi/<?= EntVideos::getIdVideoYoutube($video->txt_url) ?>/mqdefault.jpg') ">
+                                <source src="http://img.youtube.com/vi/<?= EntVideos::getIdVideoYoutube($video->txt_url) ?>/mqdefault.jpg">
+                                <!-- <source src="video.ogg" type="video/ogg">
+                                <source src="video.webm" type="video/webm"> -->
+                                Tu navegar no soporta la etiqueta de video.
+                            </video> 
+
+                            <figcaption class="overlay-panel overlay-background overlay-fade overlay-icon">
+                                <a class="icon wb-play mfp-iframe" href="<?= $video->txt_url ?>"></a>
+                                <p class="card-block"><?= $video->txt_nombre ?></p>
+                            </figcaption>
+                        </figure>
+                    </div>
             
                 <?php
             }
             ?>
+            </div>
+
         </div>
         
     </div>
